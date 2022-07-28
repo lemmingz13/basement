b15c26c42b50f207d4b7c290795a141045948330
lemmingz13 <95935262+lemmingz13@users.noreply.github.com>
2022-07-28 22:45:58 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,5 +12,36 @@ $(function(){
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
       });
+
+      let burger = document.querySelector('.burger');
+      let burgerMenu = document.querySelector('.burger_menu');
+      let burgerBack = document.querySelector('.burger_menu_back');
+
+      function burgerAction() {
+        burger.classList.toggle('burger_action');
+        burgerMenu.classList.toggle('burger_menu_active');
+        burgerBack.classList.toggle('burger_menu_back_active');
+        document.querySelector('body').classList.toggle('noscroll');
+      }
+
+      burger.addEventListener('click', burgerAction);
+
+      let menuItem = document.querySelectorAll('.burger_menu_button');
+
+      menuItem.forEach((el) => {
+        el.addEventListener('click', burgerAction);
+      });
+
+      burgerBack.addEventListener('click', burgerAction);
+
 });
